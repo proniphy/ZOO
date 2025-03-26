5599fd38d0c76db12d643387008a1a3ab165b218
Hris Panoski <hrpanoski@gmail.com>
2025-03-26 16:11:59 +0200

    Added age check for animal

--- Animal.cs
+++ Animal.cs
@@ -2,10 +2,22 @@ using System.Dynamic;
 
 class Animal
 {
+    private int age;
     public string Name { get; set; }
     public int Age { get; private set; }
-    public int Weight {get; private set; }
-
+    public int Weight 
+    {
+        get {return age;}
+        private set
+        {
+            if(age<0)
+                Console.WriteLine("Animal can't be under age of 0 years.");
+            else if(age>100)
+                Console.WriteLine("Animals can't be over the age of 150 years.");
+            else   
+                Age=value;
+        } 
+    }
     public Animal(string name, int age, int weight)
     {
         this.Name = name;
