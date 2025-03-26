cb16a58d73537fe8363c7989e085ead18bd9b363
Hristiyan Panoski <hrpanoski@gmail.com>
2025-03-26 21:02:38 +0200

    Added check for Weight, ShowAnimalName function and ShowAnimalInformation func

--- Animal.cs
+++ Animal.cs
@@ -3,9 +3,9 @@ using System.Dynamic;
 class Animal
 {
     private int age;
+    private int weight;
     public string Name { get; set; }
-    public int Age { get; private set; }
-    public int Weight 
+    public int Age
     {
         get {return age;}
         private set
@@ -18,10 +18,32 @@ class Animal
                 Age=value;
         } 
     }
+    public int Weight
+    {
+        get {return weight;}
+        private set
+        {
+            if(weight<0)
+                Console.WriteLine("Animal can't weigh less than 0 kg.");
+            else if(weight>100)
+                Console.WriteLine("Animals can't weigh over 4500 kg.");
+            else   
+                Age=value;
+        }
+    }
     public Animal(string name, int age, int weight)
     {
         this.Name = name;
         this.Age = age;
         this.Weight= weight;
+    }
+    
+    public void ShowAnimalName()
+    {
+        Console.WriteLine($"The animal's name is: {Name}");
+    }
+    public void ShowAnimalInformation()
+    {
+        Console.WriteLine($"This animal is: {Name}, it's age is {Age} and it weighs {Weight}");
     }
 }
