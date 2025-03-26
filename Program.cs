eff4639994dcf04c5b146d068128689405857bce
Hristiyan Panoski <hrpanoski@gmail.com>
2025-03-26 21:30:14 +0200

    QoL functs in main, bug fix and Mammal subclass

--- Program.cs
+++ Program.cs
@@ -183,4 +183,25 @@ class Program
         }
         return animalCatalog;
     }
+    public static int GetIntInput(string message) //Function to get Int, also checks for correct input
+    { 
+        int value; 
+        do
+        {
+            Console.Write(message); 
+        }while(!int.TryParse(Console.ReadLine(), out value)||value<0);
+        
+        return value; 
+    }
+    public static string GetStringInput(string message) //analog to GetIntInput for Strings.
+    { 
+        string? value; 
+        do
+        {
+            Console.Write(message);
+            value = Console.ReadLine();
+        } while (string.IsNullOrWhiteSpace(value));
+    
+    return value;
+    }
 }
