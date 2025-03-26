63354f8404e3ed18227f630e6d8c5c0710439b3a
Hris Panoski <hrpanoski@gmail.com>
2025-03-26 16:07:22 +0200

    Edited Main class

--- Animal.cs
+++ Animal.cs
@@ -1,10 +1,15 @@
+using System.Dynamic;
+
 class Animal
 {
     public string Name { get; set; }
     public int Age { get; private set; }
-    public Animal(string name, int age)
+    public int Weight {get; private set; }
+
+    public Animal(string name, int age, int weight)
     {
         this.Name = name;
         this.Age = age;
+        this.Weight= weight;
     }
 }
