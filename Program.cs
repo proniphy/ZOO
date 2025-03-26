231cdbe105afbbe6ec03bf100809c652cf782735
Hris Panoski <hrpanoski@gmail.com>
2025-03-26 16:00:01 +0200

    added main func

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Dynamic;
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
