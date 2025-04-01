71e72d4ea0c0b6a445e975787b7075b73260f803
Plamen <plamenmateev1@gmail.com>
2025-04-01 21:41:53 +0300

    Initial push: Added Catalog class and added functionality to add mammals and birds.

--- Bird/Bird.cs
+++ Bird/Bird.cs
@@ -1,8 +1,15 @@
 class Bird : Animal 
 {
-    public string Flying {get;set;}
-    public Bird(string name, int age, string flying) : base(name, age)
+    public bool Flying {get;set;}
+
+    public Bird(string name, int age, bool flying, double weight) : base(name, age, weight)
     {
         this.Flying=flying;
+    }
+
+    public void ShowBirdInformation()
+    {
+        string canFly = Flying ? "It can fly." : "It can't fly.";
+        Console.WriteLine($"{base.ShowAnimalInformation()}. {canFly}");
     }
 }
---
+++ Catalog.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Catalog
+{
+    private List<Mammal> mammalList = new List<Mammal>();
+    private List<Bird> birdList = new List<Bird>();
+
+    public void AddMammal(Mammal mammal)
+    {
+        mammalList.Add(mammal);
+    }
+
+    public void AddBird(Bird bird)
+    {
+        birdList.Add(bird);
+    }
+
+    public void DisplayMammalCatalog()
+    {    
+        if (mammalList.Any())
+        {
+            Console.WriteLine("Mammal Catalog: ");
+            Mammal.MammalInformation();
+            foreach (var mammal in mammalList)
+            {
+                mammal.ShowMammalInformation();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Currently there are no mammals in this catalog.");
+        }
+    }
+
+    public void DisplayBirdCatalog()
+    {
+        if (birdList.Any())
+        {
+            Console.WriteLine("Bird Catalog: ");
+            foreach (var bird in birdList)
+            {
+                bird.ShowBirdInformation();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Currently there are no birds in this catalog.");
+        }
+    }
+}
