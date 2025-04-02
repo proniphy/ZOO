ed08225e66aa5dae3b9be918a311d9145f9b8f26
teonikolov2002 <93723270+teonikolov2002@users.noreply.github.com>
2025-04-02 14:21:58 +0300

    Fix Reptile

---
+++ reptiles/Reptile.cs
@@ -0,0 +1,42 @@
+class Reptile : Animal
+{
+    private int legs;
+    private string habitat;
+    public int Legs
+    {
+        get {return legs;}
+        private set
+        {
+            if(legs<0)
+            {
+                Console.WriteLine("A reptile cannot have less than 0 legs.");
+            } else
+            {
+                legs=value;
+            }
+        }
+    }
+    public string? Habitat
+    {
+        get {return habitat;}
+        private set {havitat=value;}
+    }
+    public Reptile(string name, int age, double weight, int legs, string habitat) : base(name, age, weight)
+    {
+        this.Legs = legs;
+        this.Habitat = habitat;
+    }
+    public void Sunbathe()
+    {
+        Console.WriteLine("The Reptile is sunbathing currently...");
+        Console.WriteLine("This is done so that the reptile regulates its internal temperature.");
+    }
+    public static void InformationAboutReptiles()
+    {
+        Console.WriteLine("1. The reptiles are air-breathing vertebrates.");
+        Console.WriteLine("2. The reptiles' skin is either made up of bony plates or scales. ");
+        Console.WriteLine("3. There are many types of reptiles, such as lizards, tortoises, turtles, snakes and many more.");
+        Console.WriteLine("4. Important dearure is that reptiles cannot regulate their body temperature internally.");
+        Console.WriteLine("5. Most reptiles reproduce via laying eggs, however, some give birth to their offspring.");
+    }
+}
