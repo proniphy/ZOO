4f5677b9fc60356a232f59decc84cf53ce4e58b4
Ivan Tabashki <12474308+tabashki@users.noreply.github.com>
2025-04-08 15:47:57 +0300

    Update Bird, Mammal to implement all new interfaces

--- Bird/Bird.cs
+++ Bird/Bird.cs
@@ -1,16 +1,36 @@
-class Bird : Animal
+class Bird : Animal, IVocalize, ISpeciesInfo
 {
-    public bool Flying {get;set;}
+    public bool Flying { get; protected set; }
 
-    public Bird(string name, DateTime birthDate, double weightInKg, bool flying)
-        : base(name, birthDate, weightInKg)
+
+    public Bird(string name, DateTime birthDate, double weightInKg,
+        HabitatTypes habitat, string? originRegion = null, bool flying = true)
+        : base(name, birthDate, weightInKg, habitat, originRegion)
     {
-        this.Flying=flying;
+        Flying = flying;
+    }
+
+    public override string GetAnimalInformation()
+    {
+        string canFly = Flying ? "Additionaly, it can fly" : "Sadly, it can't fly";
+        return $"{base.GetAnimalInformation()}. {canFly}.";
     }
 
     public void ShowBirdInformation()
     {
-        string canFly = Flying ? "It can fly." : "It can't fly.";
-        Console.WriteLine($"{base.ShowAnimalInformation()}. {canFly}");
+        base.ShowAnimalInformation();
+    }
+
+    public virtual void Vocalize()
+    {
+        Console.WriteLine("Chirp, chirp!");
+    }
+
+    public static string GetSpeciesInfo()
+    {
+        return "Birds are warm-blooded vertebrates characterized by feathers, " +
+            "toothless beaked jaws, the laying of hard-shelled eggs, " +
+            "and a high metabolic rate. They range in size from tiny " +
+            "hummingbirds to large ostriches.";
     }
 }
