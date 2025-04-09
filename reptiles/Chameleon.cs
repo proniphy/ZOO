1d0ca72da037a21a02144993e209a1007cc76914
teonikolov2002 <93723270+teonikolov2002@users.noreply.github.com>
2025-04-09 14:40:27 +0300

    TN_Reptiles_Revised_3

--- reptiles/Chameleon.cs
+++ reptiles/Chameleon.cs
@@ -11,13 +11,20 @@ class Chameleon : Reptile, ICamouflaging
         Console.WriteLine($"{Name} makes soft clicking sounds and hisss");
     }
 
-    public override void ShowAnimalInformation()
+    public override void GetAnimalInformation()
     {
-        Console.WriteLine("1. It's a color-shifting chameleon. While most people think chameleons change color only to blend into their environment,");
-        Console.WriteLine("     they actually do it to communicate emotions like aggression, fear, or readiness to mate. Temperature and light also influence the change.");
-        Console.WriteLine("2. A chameleon's tongue can be up to twice the length of its body and can launch out at lightning speed—up to 0.07 seconds—to snatch insects with sticky precision.");
-        Console.WriteLine("3. Chameleons have independently rotating eyes—each eye can move in a different direction. This gives them a full 360° field of vision ");
-        Console.WriteLine("     and precise depth perception for spotting prey.");
+        string info = $"This animal is named: {Name}, it is {AgeInYears} years old, " +
+            $"  it weighs {Weight} kg and is native to the {NativeHabitat.FriendlyName()} habitat. \n" + 
+            $"  1. It's a color-shifting chameleon. While most people think chameleons change color only to blend into their environment, \n" +
+            "they actually do it to communicate emotions like aggression, fear, or readiness to mate. Temperature and light also influence the change. \n" +
+            "   2. A chameleon's tongue can be up to twice the length of its body and can launch out at lightning speed—up to 0.07 seconds—to snatch insects with sticky precision. \n" +
+            "   3. Chameleons have independently rotating eyes—each eye can move in a different direction. This gives them a full 360° field of vision \n" +
+            "and precise depth perception for spotting prey. \n"
+        if (!string.IsNullOrEmpty(RegionOfOrigin))
+        {
+            info += $" It was captured from the region {RegionOfOrigin}. ";
+        }
+        return info;
     }
 
     public void Camouflage()
--- reptiles/Turtle.cs
+++ reptiles/Turtle.cs
@@ -15,12 +15,19 @@ class Turtle : Reptile, ISwimmer
         Console.WriteLine($"{Name} makes a low grunting or hissing sound.");
     }
 
-    public override void ShowAnimalInformation()
+    public override string GetAnimalInformation()
     {
-        Console.WriteLine($"1. It's a turtle with a shell and {(CanSwimUnderwater ? "can swim underwater" : "prefers land")}. ");
-        Console.WriteLine("2. Some species, like the Galápagos tortoise, can live over 100 years. Turtles have slow metabolisms, which contributes to their long lifespans.");
-        Console.WriteLine("3. While they have lungs, some turtles can absorb oxygen through their skin or cloaca (rear end)—a handy trick for underwater survival, especially during hibernation.");
-        Console.WriteLine("4. A turtle’s shell isn’t just armor—it's actually fused with their spine and ribs, making it a permanent, living part of their body. They cannot crawl out of it like in cartoons.");
+        string info = $"This animal is named: {Name}, it is {AgeInYears} years old, " +
+            $"it weighs {Weight} kg and is native to the {NativeHabitat.FriendlyName()} habitat. \n" + 
+            $"1. It's a turtle with a shell and {(CanSwimUnderwater ? "can swim underwater" : "prefers land")}. \n" +
+            "2. Some species, like the Galápagos tortoise, can live over 100 years. Turtles have slow metabolisms, which contributes to their long lifespans. \n" +
+            "3. While they have lungs, some turtles can absorb oxygen through their skin or cloaca (rear end)—a handy trick for underwater survival, especially during hibernation. \n" +
+            "4. A turtle’s shell isn’t just armor—it's actually fused with their spine and ribs, making it a permanent, living part of their body. They cannot crawl out of it like in cartoons. \n"
+        if (!string.IsNullOrEmpty(RegionOfOrigin))
+        {
+            info += $" It was captured from the region {RegionOfOrigin}. ";
+        }
+        return info;
     }
 
     public void Swim()
