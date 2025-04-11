9c88d8a5870fbb83b85c3eba9a490693c9e3f558
Hristiyan Panoski <hrpanoski@gmail.com>
2025-04-11 22:32:27 +0300

    Added abstract Mammal class and couple of functions in Animal.cs

--- mammals/Mammals.cs
+++ mammals/Mammals.cs
@@ -1,20 +1,54 @@
-class Mammal : Animal, ISpeciesInfo
+abstract class Mammal :   Animal, IVocalize
 {
-    public bool IsWarmBlooded {get; set; } = true; //Mammals are warm blooded
+    //All animals are warm blooded
+    public bool IsWarmBlooded {get;set;} = true;
+    private double averageBodyTemperature;
 
-    public Mammal(string name, DateTime birthDate, double weightInKg,
-        HabitatTypes habitat, string? originRegion = null, bool warmBlooded = true)
-        : base(name, birthDate, weightInKg, habitat, originRegion)
+    //Fur type e.g. none for dolphins, long for alpaca/sheep, etc...
+    public string? FurType {get; set;} 
+    //Ear type e.g. big for elephant, pointy for lynx, etc..
+    public string? EarType {get; set;}
+    //Tail type e.g. no tail for baboons, short/bushy for some type of cats
+    public string? TailType{get; set;}
+
+    public double AverageBodyTemperature
     {
-        IsWarmBlooded = warmBlooded;
+        get => averageBodyTemperature;
+        set
+        {
+            if(value<30||value>45)
+                throw new ArgumentOutOfRangeException("Temperature out of expected mammalian range.");
+            else
+                averageBodyTemperature=value;
+        }
+    }
+    //mammal without any arguments.
+    public Mammal() : base()
+    {
+        AverageBodyTemperature = 37.0;
+        FurType="Unspecified.";
+        EarType="Unspecified.";
+        TailType="Unspecified";
+    }
+    //actual mammal constuctor that could be pointed to later on.
+    public Mammal(string name, DateTime birthDate, double weightInKg, HabitatTypes habitat, string origin
+    , double averageBodyTemperature, string furType, string earType, string tailType)
+    : base(name, birthDate, weightInKg, habitat, origin)
+    {
+        AverageBodyTemperature=averageBodyTemperature;
+        FurType=furType;
+        EarType=earType;
+        TailType=tailType;
     }
 
-    public static bool IsMammal(Animal animal) //returns true or false
+   public abstract void Vocalize();
+
+    public override string GetAnimalClass()
     {
-        return animal is Mammal; //depending on if animal is or isn't a mammal
+        return "Mammal";
     }
 
-    public static string GetSpeciesInfo() //Basic mammal info
+    public override string GetAnimalClassInfo() //Basic mammal info
     {
         return "Most mammals are distinct by these characteristics:\n" +
             "  1. They're warm-blooded animals and typically have hair or fur.\n" +
@@ -24,11 +58,18 @@ class Mammal : Animal, ISpeciesInfo
             "  5. We can categorize mammals based on their diet in three groups:\n" +
             "    5.1. Carnivores.\n" +
             "    5.2. Herbivores.\n" +
-            "    5.3. Omnivores.";
+            "    5.3. Omnivores."; 
     }
-
-    public void ShowMammalInformation()
+    public override string GetAnimalInformation() //points to GetAnimalInformation in Animal.cs and adds on the extra info.
     {
-        base.ShowAnimalInformation();
+        string info=base.GetAnimalInformation();
+
+        info+=  $"\nJust like every mammal it is warm blooded," +
+                $"it has average body temperature: {AverageBodyTemperature}°C,\n " +
+                $"it has {EarType} ears, " +
+                $"also has {FurType} fur, " +
+                $"and a {TailType} tail.";
+
+        return info;
     }
 }
