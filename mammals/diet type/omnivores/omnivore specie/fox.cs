4f1eae00086df8a02ac6db684fdfcd4075599a6e
alexanderchernev <a.chernev009@gmail.com>
2025-04-14 14:46:43 +0300

    Added a folder "omnivore specie" with some of the omnivore mammals

---
+++ mammals/diet type/omnivores/omnivore specie/fox.cs
@@ -0,0 +1,14 @@
+class Fox : Omnivore
+{
+    public Fox(string name, DateTime birthDate, double weightInKg, HabitatTypes habitat, string origin,
+        double averageBodyTemperature, string furType, string earType, string tailType,
+        double foodConsumptionPerDayKg, string favouritePlantFood, string favouriteAnimalFood)
+        : base(name, birthDate, weightInKg, habitat, origin, averageBodyTemperature, furType, earType, tailType,
+              foodConsumptionPerDayKg, favouritePlantFood, favouriteAnimalFood)
+    { }
+
+    public override void Vocalize()
+    {
+        Console.WriteLine("The fox barks softly and makes a yipping sound.");
+    }
+}
