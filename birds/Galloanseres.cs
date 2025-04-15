673d2df7fe223f4b9e4d9d06c9fde9efed59b85b
Maria-Desimira <maria.desimira@gmail.com>
2025-04-15 23:42:35 +0300

    birds orders types

---
+++ birds/Galloanseres.cs
@@ -0,0 +1,12 @@
+abstract class Galloansetes : Bird
+{
+    public Galloanseres(string name, DateTime birthDate, double weightInKg, 
+        HabitatTypes habitat, string? originRegion = null, bool flying = true)
+        : base(name, birthDate, weightInKg, habitat, originRegion, flying) { }
+
+    public override string BirdType => "Galloanseres";
+    public override void Identify() 
+    {
+        Console.WriteLine("It is a Galloanseres bird!");
+    }
+}
